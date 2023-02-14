79d22357415036a699840e043627ce683b1f7f22
gangan <jacky.hong88@gmail.com>
2023-02-14 17:59:44 +0800

    Create WebAppCourse3 page

---
+++ WebAppCourse3/script.js
@@ -0,0 +1,19 @@
+// 獲取網址當中的參數
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+    return "";
+};
+
+var param = getUrlParameter("name");
+var name = document.getElementById("name").innerHTML = param;
